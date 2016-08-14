8fb1267fa7060fa3008113963ae407a73cd419a8
Neil Paul <neilcpaul@gmail.com>
2016-08-14 17:22:17 +0100

    Commit of simple console reader

---
+++ cpp_consolereader/reader.cpp
@@ -0,0 +1,15 @@
+// np
+// Simple console reader
+// 20160814
+
+#include <iostream>
+
+int main() {
+	std::cout << "Enter two numbers:" << std::endl;
+	int v1, v2;
+	std::cin >> v1 >> v2;
+	std::cout << "The sum of " << v1 << " and " << v2
+			  << " is " << v1 + v2 << std::endl;
+
+	return 0;
+}
